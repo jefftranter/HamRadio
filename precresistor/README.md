fb110553db96ed1b29f03bb52de016738a7ff8bf
Jeff Tranter <tranter@pobox.com>
2021-01-18 17:49:21 -0500

    Initial work on application.

---
+++ precresistor/README.md
@@ -0,0 +1,8 @@
+A Qt-based applicatons for calculating values of precision resistors
+using several standard values in parallel or series.
+
+Requires the Qt framework.
+
+Builds using cmake.
+
+Still a work in progress.
---
+++ precresistor/precresistor_fr_CA.ts
@@ -0,0 +1,235 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_CA">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="302"/>
+        <source>Precision Resistor Calculator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="25"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="305"/>
+        <source>Mode:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="32"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="313"/>
+        <source>Select desired type of circuit.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="36"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="306"/>
+        <source>Series (2)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="41"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="307"/>
+        <source>Series (3)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="46"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="308"/>
+        <source>Parallel (2)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="51"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="309"/>
+        <source>Parallel (3)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="56"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="310"/>
+        <source>Series/Parallel (3)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="68"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="315"/>
+        <source>Standard Values:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="75"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="324"/>
+        <source>Select desired resistor series.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="79"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="316"/>
+        <source>E6 (20%)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="84"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="317"/>
+        <source>E12 (10%)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="89"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="318"/>
+        <source>E24 (5%)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="94"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="319"/>
+        <source>E48 (2%)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="99"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="320"/>
+        <source>E96 (1%)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="104"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="321"/>
+        <source>E192 (0.5%)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="116"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="326"/>
+        <source>Desired Value:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="123"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="328"/>
+        <source>Enter desired resistance value.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="131"/>
+        <location filename="mainwindow.ui" line="164"/>
+        <location filename="mainwindow.ui" line="206"/>
+        <location filename="mainwindow.ui" line="248"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="330"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="335"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="344"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="353"/>
+        <source>Ohms</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="136"/>
+        <location filename="mainwindow.ui" line="169"/>
+        <location filename="mainwindow.ui" line="211"/>
+        <location filename="mainwindow.ui" line="253"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="331"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="336"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="345"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="354"/>
+        <source>Kilohms</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="141"/>
+        <location filename="mainwindow.ui" line="174"/>
+        <location filename="mainwindow.ui" line="216"/>
+        <location filename="mainwindow.ui" line="258"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="332"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="337"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="346"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="355"/>
+        <source>Megohms</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="153"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="334"/>
+        <source>R1:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="182"/>
+        <location filename="mainwindow.ui" line="224"/>
+        <location filename="mainwindow.ui" line="266"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="340"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="349"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="358"/>
+        <source>Check to calculate value, uncheck to enter it.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="185"/>
+        <location filename="mainwindow.ui" line="227"/>
+        <location filename="mainwindow.ui" line="269"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="342"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="351"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="360"/>
+        <source>Calculate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="195"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="343"/>
+        <source>R2:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="237"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="352"/>
+        <source>R3:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="283"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="361"/>
+        <source>&lt;b&gt;Actual Value:&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="290"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="362"/>
+        <source>&lt;b&gt;889 Ohms (+0.05%)&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="301"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="364"/>
+        <source>Click to calculate best design.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="304"/>
+        <location filename="mainwindow.ui" line="382"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="304"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="366"/>
+        <source>&amp;Calculate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="324"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="368"/>
+        <source>Click to quit application.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="327"/>
+        <location filename="mainwindow.ui" line="377"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="303"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="370"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="367"/>
+        <location filename="build/precresistor_autogen/include/ui_mainwindow.h" line="372"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
