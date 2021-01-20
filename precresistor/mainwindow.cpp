89efba169e1e53267a15c5adb7037ba71fa25c72
Jeff Tranter <tranter@pobox.com>
2021-01-20 18:44:12 -0500

    More enhancements.

--- precresistor/mainwindow.cpp
+++ precresistor/mainwindow.cpp
@@ -1,8 +1,8 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 #include <QDebug>
-#include <QString>
 #include <QMessageBox>
+#include <QString>
 #include <math.h>
 
 // List of standard resistor values.
@@ -14,8 +14,6 @@ const int e12Series[] = {0, 10, 12, 15, 18, 22, 27, 33, 39, 47, 56, 68, 82};
 
 const int e24Series[] = {0,  10, 11, 12, 13, 15, 16, 18, 20, 22, 24, 27, 30,
                          33, 36, 39, 43, 47, 51, 56, 62, 68, 75, 82, 91};
-
-const int e24Decades[] = {0, 1, 2, 3, 4, 5, 6};
 
 const int e48Series[] = {0,   100, 105, 110, 115, 121, 127, 133, 140, 147,
                          154, 162, 169, 178, 187, 196, 205, 215, 226, 237,
@@ -46,6 +44,9 @@ const int e192Series[] = {
     597, 604, 612, 619, 626, 634, 642, 649, 657, 665, 673, 681, 690, 698, 706,
     715, 723, 732, 741, 750, 759, 768, 777, 787, 796, 806, 816, 825, 835, 845,
     856, 866, 876, 887, 898, 909, 920, 931, 942, 953, 965, 976, 988};
+
+const int firstDecade = -1;
+const int lastDecade = 6;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
@@ -129,13 +130,20 @@ void MainWindow::calculate() {
   double bestR2 = -1;
   double bestDiff = 1e6;
 
-  //qDebug() << "Desired value is" << desiredValue << "Ohms.";
+  if (desiredValue > 100e6) {
+    QMessageBox::warning(this, tr("Out of Range"),
+                         tr("The desired value is too high, please specify a "
+                            "value of no more than 100 Megohms."));
+    return;
+  }
+
+  // qDebug() << "Desired value is" << desiredValue << "Ohms.";
 
   // Calculate by brute force.
   int len = sizeof(e24Series) / sizeof(e24Series[0]);
-  for (int decade1 = -1; decade1 <= 6; decade1++) {
+  for (int decade1 = firstDecade; decade1 <= lastDecade; decade1++) {
     for (int i1 = 0; i1 < len; i1++) {
-      for (int decade2 = -1; decade2 <= 6; decade2++) {
+      for (int decade2 = firstDecade; decade2 <= lastDecade; decade2++) {
         for (int i2 = 0; i2 < len; i2++) {
           double r1 = e24Series[i1] * exp10(decade1);
           double r2 = e24Series[i2] * exp10(decade2);
@@ -152,8 +160,8 @@ void MainWindow::calculate() {
     }
   }
 
-  //qDebug() << "Best R1 =" << bestR1;
-  //qDebug() << "Best R2 =" << bestR2;
+  // qDebug() << "Best R1 =" << bestR1;
+  // qDebug() << "Best R2 =" << bestR2;
 
   if (bestR1 > 1e6) {
     ui->r1ResistanceComboBox->setCurrentIndex(2); // Megohms
@@ -181,84 +189,79 @@ void MainWindow::calculate() {
   double error = (value - desiredValue) / desiredValue;
   QString s;
   if (value > 1e6) {
-    s = tr("<b>") + QString::number(value / 1e6) + tr(" Megohms");
+    s = tr("<b>%1 Megohms").arg(QString::number(value / 1e6));
   } else if (value > 1e3) {
-    s = tr("<b>") + QString::number(value / 1e3) + tr(" Kilohms");
+    s = tr("<b>%1 Kilohms").arg(QString::number(value / 1e3));
   } else {
-    s = tr("<b>") + QString::number(value) + tr(" Ohms");
+    s = tr("<b>%1 Ohms").arg(QString::number(value));
   }
-  s += tr(" (error ") + QString::number(100 * error, 'g', 2) + tr("%)</b>");
+  s += tr(" (error %1%)</b>").arg(QString::number(100 * error, 'g', 2));
   ui->actualValueLabel->setText(s);
 }
 
+void MainWindow::info() {
+  QString text;
+  const int *series = nullptr;
+  int size = 0;
 
-void MainWindow::info()
-{
-    QString text;
-    const int *series;
-    int size;
+  switch (ui->standardValuesComboBox->currentIndex()) {
+  case 0:
+    text = tr("Standard resistor values in E6 (20%) series:\n\n");
+    series = e6Series;
+    size = sizeof(e6Series) / sizeof(e6Series[0]);
+    break;
+  case 1:
+    text = tr("Standard resistor values in E12 (10%) series:\n\n");
+    series = e12Series;
+    size = sizeof(e12Series) / sizeof(e12Series[0]);
+    break;
+  case 2:
+    text = tr("Standard resistor values in E24 (5%) series:\n\n");
+    series = e24Series;
+    size = sizeof(e24Series) / sizeof(e24Series[0]);
+    break;
+  case 3:
+    text = tr("Standard resistor values in E48 (2%) series:\n\n");
+    series = e48Series;
+    size = sizeof(e48Series) / sizeof(e48Series[0]);
+    break;
+  case 4:
+    text = tr("Standard resistor values in E96 (1%) series:\n\n");
+    series = e96Series;
+    size = sizeof(e96Series) / sizeof(e96Series[0]);
+    break;
+  case 5:
+    text = tr("Standard resistor values in E192 (0.5%) series:\n\n");
+    series = e192Series;
+    size = sizeof(e192Series) / sizeof(e192Series[0]);
+    break;
+  }
 
-    switch(ui->standardValuesComboBox->currentIndex())
-    {
-    case 0:
-        text = "Standard resistor values in E6 (20%) series:\n\n";
-        series = e6Series;
-        size = sizeof(e6Series) / sizeof(e6Series[0]);
-        break;
-    case 1:
-        text = "Standard resistor values in E12 (10%) series:\n\n";
-        series = e12Series;
-        size = sizeof(e12Series) / sizeof(e12Series[0]);
-        break;
-    case 2:
-        text = "Standard resistor values in E24 (5%) series:\n\n";
-        series = e24Series;
-        size = sizeof(e24Series) / sizeof(e24Series[0]);
-        break;
-    case 3:
-        text = "Standard resistor values in E48 (2%) series:\n\n";
-        series = e48Series;
-        size = sizeof(e48Series) / sizeof(e48Series[0]);
-        break;
-    case 4:
-        text = "Standard resistor values in E96 (1%) series:\n\n";
-        series = e96Series;
-        size = sizeof(e96Series) / sizeof(e96Series[0]);
-        break;
-    case 5:
-        text = "Standard resistor values in E192 (0.5%) series:\n\n";
-        series = e192Series;
-        size = sizeof(e192Series) / sizeof(e192Series[0]);
-        break;
+  for (int i = 1; i < size; i++) {
+    text += QString::number(series[i]);
+    if (i % 12) {
+      text += tr(" ");
+    } else {
+      text += tr("\n");
     }
+  }
 
-    for (int i = 1; i < size; i++) {
-        text += QString::number(series[i]);
-        if (i % 12) {
-            text += " ";
-        } else {
-            text += "\n";
-        }
-    }
-
-    QMessageBox::information(this, tr("Info"), text);
+  QMessageBox::information(this, tr("Info"), text);
 }
 
-void MainWindow::about()
-{
-    QMessageBox::about(
-                       this,
-                       tr("About Precresistor"),
-                       tr("Precision Resistor application by Jeff Tranter.\n\n" \
-                          "Copyright (C) 2021 by Jeff Tranter <tranter@pobox.com>.\n\n" \
-                          "Licensed under the Apache License, Version 2.0 (the \"License\");\n" \
-                          "you may not use this file except in compliance with the License.\n" \
-                          "You may obtain a copy of the License at\n\n" \
-                          "  http://www.apache.org/licenses/LICENSE-2.0\n\n" \
-                          "Unless required by applicable law or agreed to in writing, software\n" \
-                          "distributed under the License is distributed on an \"AS IS\" BASIS,\n" \
-                          "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n" \
-                          "See the License for the specific language governing permissions and\n" \
-                          "limitations under the License.\n")
-                       );
+void MainWindow::about() {
+  QMessageBox::about(
+      this, tr("About Precresistor"),
+      tr("<b>Precision Resistor application by Jeff Tranter.</b><br><br>"
+         "Copyright (C) 2021 by Jeff Tranter &lt;tranter@pobox.com&gt;.<br><br>"
+         "Licensed under the Apache License, Version 2.0 (the \"License\");<br>"
+         "you may not use this file except in compliance with the License.<br>"
+         "You may obtain a copy of the License at<br><br>"
+         "  http://www.apache.org/licenses/LICENSE-2.0<br><br>"
+         "Unless required by applicable law or agreed to in writing, software<br>"
+         "distributed under the License is distributed on an \"AS IS\" BASIS,<br>"
+         "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or "
+         "implied.<br>"
+         "See the License for the specific language governing permissions and<br>"
+         "limitations under the License.<br>"));
 }
